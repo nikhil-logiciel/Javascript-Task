d5a0aead38b04f6796a28c1dff7b3185c1dbeabe
pawan-logiciel <87014932+nikhil-logiciel@users.noreply.github.com>
2021-08-26 18:34:47 +0530

    Javascript Assignment
    Here, I have updated my task that include -
    1.)Creating Form having input fields named Firstname, Lastname, Age, Address.
    2.)Adding the input field data to the List View by using Dom targeting method.
    3.)Using Clear button to clear the input fields.
    4.)Added Validation to the input fields.

---
+++ index.js
@@ -0,0 +1,37 @@
+var value = []
+
+function add(){
+   const b = {};
+   b["fname"]  = document.getElementById("fname").value;
+   b["lname"]= document.getElementById("lname").value;
+   b["age"]= document.getElementById("age").value;
+   b["address"] = document.getElementById("address").value;
+   // let newvalue = {Firstname, Lastname, Age, Address};
+   value.push(b);
+   console.log(value);
+  
+   var newElement = document.createElement("li");   // Create a <button> element
+   var s = '<div></div>'
+   newElement.innerHTML ="Firstname-" + b.fname  + "<br>" + "Lastname-" + b.lname + "<br>" + "Age-" + b.age  + "<br>" + "Address-" +b.address;                   // Insert text
+   document.getElementById("list").appendChild(newElement);  
+}
+function clrfrm(){
+   document.getElementById('form1').reset();
+}
+function validate(){
+   var fname = document.getElementById("fname");
+   var lname = document.getElementById("lname");
+   var age = document.getElementById("age");
+   var address = document.getElementById("address");
+
+   if (fname.value =="" || lname.value ==""){
+      alert("Input Field Empty.")
+   }
+   else if (age.value =="" || address.value ==""){
+      alert("Input Field Empty.")
+   }
+   else{
+
+   }
+}
+       

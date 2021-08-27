e3c58598b0fc2166b11c69ac7571ceee27aa318e
pawan-logiciel <87014932+nikhil-logiciel@users.noreply.github.com>
2021-08-27 18:42:04 +0530

    Javascript Assignment.
    Here, I added the below given things today;
    1.)All input fields data to list item
    2.)Creating Edit and Delete Buttons to the each list item.
    3.)Using Edit and Delete Buttons to update/delete any item of the list.

--- index.js
+++ index.js
@@ -6,14 +6,23 @@ function add(){
    b["lname"]= document.getElementById("lname").value;
    b["age"]= document.getElementById("age").value;
    b["address"] = document.getElementById("address").value;
+   b["id"] = document.getElementById("id").value;
    // let newvalue = {Firstname, Lastname, Age, Address};
    value.push(b);
    console.log(value);
   
    var newElement = document.createElement("li");   // Create a <button> element
    var s = '<div></div>'
-   newElement.innerHTML ="Firstname-" + b.fname  + "<br>" + "Lastname-" + b.lname + "<br>" + "Age-" + b.age  + "<br>" + "Address-" +b.address;                   // Insert text
+   newElement.innerHTML ="Firstname-" + b.fname  + "<br>" + "Lastname-" + b.lname + "<br>" + "Age-" + b.age  + "<br>" + "Address-" +b.address;           
    document.getElementById("list").appendChild(newElement);  
+
+   var edit = document.createElement("button");
+   var eText = document.createTextNode("\u270E");
+   edit.className = "edit";
+   edit.appendChild(eText);
+   list.appendChild(edit);
+
+   
 }
 function clrfrm(){
    document.getElementById('form1').reset();
@@ -31,7 +40,6 @@ function validate(){
       alert("Input Field Empty.")
    }
    else{
-
    }
 }
        
